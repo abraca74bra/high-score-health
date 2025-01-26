8adea2285f6a413e8833d2ec7b1ce47f5ad98141
AngelaB <angela.brazil@outlook.com>
2025-01-25 22:42:18 -0500

    Add page load function

--- buttons.js
+++ buttons.js
@@ -1,30 +1,44 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    onLoad();
+});
+
+function onLoad() {
+    //Get the cached value of the header total
+    var runningTotal = localStorage.getItem('headerTotal');
+
+    //Populate the value in the header tracking field
+    var headerTotal = document.getElementById("header_total");
+    headerTotal.innerHTML = runningTotal;
+
+}
+
 function openTab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
-  
+
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
-  
+
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-  
+
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
-  }
+}
 
-  function addPoints(evt){
-    
+function addPoints(evt) {
+
     //Get the value from the input field
     var pointsInput = document.getElementById("add_points_input");
     var pointsToAdd = parseFloat(pointsInput.value);
-    if(!pointsToAdd){
+    if (!pointsToAdd) {
         return
     }
     //Reset the input
@@ -40,13 +54,13 @@ function openTab(evt, tabName) {
     //Populate the value in the header tracking field
     headerTotal.innerHTML = runningTotal;
     localStorage.setItem("headerTotal", runningTotal);
-  }
+}
 
-  function subtractPoints(evt){
+function subtractPoints(evt) {
     //Get the value from the input field
     var pointsInput = document.getElementById("subtract_points_input");
     var pointsToSubtract = parseFloat(pointsInput.value);
-    if(!pointsToSubtract){
+    if (!pointsToSubtract) {
         return
     }
     //Reset the input
@@ -57,24 +71,25 @@ function openTab(evt, tabName) {
     var runningTotal = parseFloat(headerTotal.innerHTML);
 
     //Calculate the new total
-    runningTotal -= pointsToAdd;
+    runningTotal -= pointsToSubtract;
 
     //Populate the value in the header tracking field
     headerTotal.innerHTML = runningTotal;
     localStorage.setItem("headerTotal", runningTotal);
-  }
+}
 
-  function resetPoints(evt){
+function resetPoints(evt) {
     //Get the value from the input field
     var pointsInput = document.getElementById("reset_points_input");
     var resetTotal = parseFloat(pointsInput.value);
-    if(!resetTotal){
+    if (!resetTotal) {
         return
     }
     //Reset the input
     pointsInput.value = '';
 
     //Populate the value in the header tracking field
+    var headerTotal = document.getElementById("header_total");
     headerTotal.innerHTML = resetTotal;
     localStorage.setItem("headerTotal", resetTotal);
-  }
+}
