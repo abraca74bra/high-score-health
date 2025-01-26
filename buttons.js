1251414ea5a7a6b6813ba6c48c270d9bbaa3bbb9
AngelaB <angela.brazil@outlook.com>
2025-01-25 21:57:46 -0500

    Add basic functions

--- buttons.js
+++ buttons.js
@@ -18,3 +18,35 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  function addPoints(evt){
+    
+    //Get the value from the input field
+    var pointsInput = document.getElementById("add_points_input");
+    var pointsToAdd = pointsInput.value;
+    if(!pointsToAdd){
+        return
+    }
+    //Reset the input
+    pointsInput.value = '';
+
+    //Get the value from the header tracking field
+    var headerTotal = document.getElementById("header_total");
+    var runningTotal = headerTotal.innerHTML;
+
+    //Calculate the new total
+    runningTotal += pointsToAdd;
+
+    //Populate the value in the header tracking field
+    headerTotal.innerHTML = runningTotal;
+  }
+
+  function subtractPoints(evt){
+    //Get the value from the header tracking field
+    
+    //Get the value from the input field
+
+    //Calculate the new total
+
+    //Populate the value in the header tracking field
+  }
