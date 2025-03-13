bea75727a078476f79dcfafbce53146bf7a2baed
AngelaB <angela.brazil@outlook.com>
2025-03-12 20:09:36 -0400

    Segregate addPoints function Add population logic

--- buttons.js
+++ buttons.js
@@ -10,16 +10,41 @@ function onLoad() {
     var headerTotal = document.getElementById("header_total");
     headerTotal.innerHTML = runningTotal;
 
-    populateTab("Earn", "earn.json");
-    populateTab("Redeem", "redeem.json");
-    populateTab("History", "history.json");
+    populateTab("Earn", "earn.json", "earn_preset");
+    populateTab("Redeem", "redeem.json", "redeem_preset");
+    populateTab("History", "history.json", "view_history");
 }
 
-function populateTab(tabId, fileName){
+function populateTab(tabId, fileName, functionName){
     console.log('Retrieving data from ' + fileName);
     
     var content = loadJSON(fileName);
     console.log(content);
+    if(!content){
+        console.log('No content');
+        return
+    }
+
+    if(typeof content == 'object'){
+        throw new Error('Loaded data is not in object format.');
+    }
+
+    var tab = document.getElementById(tabId);
+
+    for(widgetId in content){
+        var widgetData = content[widgetId];
+        
+        //Create the widget element
+        var widgetHTML = `
+            <div class="widget" onclick="${functionName}(${fileName},${widgetId})">
+                <div class="title">${widgetData.name}</div>
+                <div class="number">${widgetData.pointValue}</div>
+            </div>
+        `;
+
+        //Append the to the tab
+        tab.appendChild(widgetHTML);
+    }
 }
 
 async function loadJSON(fileName) {
@@ -58,16 +83,7 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
-function addPoints(evt) {
-
-    //Get the value from the input field
-    var pointsInput = document.getElementById("add_points_input");
-    var pointsToAdd = parseFloat(pointsInput.value);
-    if (!pointsToAdd) {
-        return
-    }
-    //Reset the input
-    pointsInput.value = '';
+function addPoints(pointsToAdd) {
 
     //Get the value from the header tracking field
     var headerTotal = document.getElementById("header_total");
@@ -81,26 +97,33 @@ function addPoints(evt) {
     localStorage.setItem("headerTotal", runningTotal);
 }
 
+function addPoints(evt) {
+
+    //Get the value from the input field
+    var pointsInput = document.getElementById("add_points_input");
+    var pointsToAdd = Math.abs(parseFloat(pointsInput.value));
+    if (!pointsToAdd) {
+        return
+    }
+
+    addPoints(pointsToAdd);
+
+    //Reset the input
+    pointsInput.value = '';
+}
+
 function subtractPoints(evt) {
     //Get the value from the input field
     var pointsInput = document.getElementById("subtract_points_input");
-    var pointsToSubtract = parseFloat(pointsInput.value);
+    var pointsToSubtract = Math.abs(parseFloat(pointsInput.value));
     if (!pointsToSubtract) {
         return
     }
+
+    addPoints(pointsToSubtract * -1);
+
     //Reset the input
     pointsInput.value = '';
-
-    //Get the value from the header tracking field
-    var headerTotal = document.getElementById("header_total");
-    var runningTotal = parseFloat(headerTotal.innerHTML);
-
-    //Calculate the new total
-    runningTotal -= pointsToSubtract;
-
-    //Populate the value in the header tracking field
-    headerTotal.innerHTML = runningTotal;
-    localStorage.setItem("headerTotal", runningTotal);
 }
 
 function resetPoints(evt) {
@@ -117,4 +140,29 @@ function resetPoints(evt) {
     var headerTotal = document.getElementById("header_total");
     headerTotal.innerHTML = resetTotal;
     localStorage.setItem("headerTotal", resetTotal);
+}
+
+function earn_present(fileName, entryId){
+    
+    //Get the entry from the file
+    var content = loadJSON(fileName);
+
+    var entry = content[entryId];
+
+    addPoints(entry.pointValue);
+    
+    //Can't do file updates (for history or use count) until we have a backend
+}
+
+function redeem_preset(fileName, entryId){
+    //Get the entry from the file
+    var content = loadJSON(fileName);
+
+    var entry = content[entryId];
+
+    addPoints(entry.pointValue * -1);
+}
+
+function view_history(entryId){
+
 }
